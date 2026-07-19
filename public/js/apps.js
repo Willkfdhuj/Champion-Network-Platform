2321080ff2d5cb23cb3ade51f966786d4f827e2d
Will <william.m.buchan@icloud.com>
2026-07-18 21:59:47 -0700

    add da apps

---
+++ public/js/apps.js
@@ -0,0 +1,131 @@
+console.log("APPS.JS LOADED");
+
+const apps = [
+
+{
+    name: "Calculator",
+    id: "calculator",
+    image: "images/calculator.jpg",
+    url: "https://www.desmos.com/scientific"
+},
+
+{
+    name: "Notes",
+    id: "notes",
+    image: "images/notes.jpg",
+    url: "https://anotepad.com/"
+},
+
+{
+    name: "Snapchat",
+    id: "snapchat",
+    image: "images/snapchat.jpg",
+    url: "https://web.snapchat.com/"
+},
+
+{
+    name: "YouTube",
+    id: "youtube",
+    image: "images/youtube.jpg",
+    url: "https://www.youtube.com/"
+},
+
+{
+    name: "Discord",
+    id: "discord",
+    image: "images/discord.jpg",
+    url: "https://discord.com/app"
+},
+
+{
+    name: "TikTok",
+    id: "tiktok",
+    image: "images/tiktok.jpg",
+    url: "https://www.tiktok.com/"
+},
+
+{
+    name: "Spotify",
+    id: "spotify",
+    image: "images/spotify.jpg",
+    url: "https://open.spotify.com/"
+},
+
+{
+    name: "GameChanger",
+    id: "gamechanger",
+    image: "images/gamechanger.jpg",
+    url: "https://web.gc.com/"
+},
+
+{
+    name: "CapCut",
+    id: "capcut",
+    image: "images/capcut.jpg",
+    url: "https://www.capcut.com/"
+},
+
+{
+    name: "SoundCloud",
+    id: "soundcloud",
+    image: "images/soundcloud.jpg",
+    url: "https://soundcloud.com/"
+},
+
+{
+    name: "ChatGPT",
+    id: "chatgpt",
+    image: "images/chatgpt.jpg",
+    url: "https://chatgpt.com/"
+},
+
+{
+    name: "Google Maps",
+    id: "maps",
+    image: "images/maps.jpg",
+    url: "https://maps.google.com/"
+},
+
+{
+    name: "Google Translate",
+    id: "translate",
+    image: "images/translate.jpg",
+    url: "https://translate.google.com/"
+},
+
+{
+    name: "Canva",
+    id: "canva",
+    image: "images/canva.jpg",
+    url: "https://www.canva.com/"
+},
+
+{
+    name: "Photopea",
+    id: "photopea",
+    image: "images/photopea.jpg",
+    url: "https://www.photopea.com/"
+},
+
+{
+    name: "GitHub",
+    id: "github",
+    image: "images/github.jpg",
+    url: "https://github.com/"
+},
+
+{
+    name: "Reddit",
+    id: "reddit",
+    image: "images/reddit.jpg",
+    url: "https://www.reddit.com/"
+},
+
+{
+    name: "Twitch",
+    id: "twitch",
+    image: "images/twitch.jpg",
+    url: "https://www.twitch.tv/"
+}
+
+];
